15bc7a8481beba44d51893fc9c3fdbe8085595f2
meryem <agzmery59@gmail.com>
2024-12-19 16:42:30 +0100

    seconde comit

--- src/main/java/exercice1/Personnage.java
+++ src/main/java/exercice1/Personnage.java
@@ -3,6 +3,7 @@ package exercice1;
 public class Personnage {
     public static String tourner(int  fois ){
         String orio="nord";
+        if(fois%4==1) orio="EST" ;
         return orio;
     }
 }
