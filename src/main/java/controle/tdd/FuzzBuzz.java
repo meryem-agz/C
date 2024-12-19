7bded970612d0e56e42ca78eacdf2cbe304983c4
meryem <agzmery59@gmail.com>
2024-12-19 20:02:30 +0100

    2nd commit

--- src/main/java/controle/tdd/FuzzBuzz.java
+++ src/main/java/controle/tdd/FuzzBuzz.java
@@ -2,7 +2,15 @@ package controle.tdd;
 
 public class FuzzBuzz {
         public static String de(int nbr) {
-            String chaine=String.valueOf(nbr) ;
+            String chaine=" " ;
+            if(nbr%3==0) {
+                if (nbr % 5 == 0) {
+                    chaine = "FizzBuzz";
+                } else chaine = "Fizz";
+            }
+            else if(nbr%5==0)
+                chaine="Buzz";
+            else chaine= String.valueOf(nbr) ;
             return chaine;
 }
     }
@@ -16,11 +24,3 @@ public class FuzzBuzz {
 
 
 
-//if(nbr%3==0) { // CYCLE2
-//        if (nbr % 5 == 0) { //CYCLE4
-//chaine = "FizzBuzz";
-//        } else chaine = "Fizz";
-//        }
-//        else if(nbr%5==0) //CYCLE3
-//chaine="Buzz";
-//        else chaine= String.valueOf(nbr) ;//cycle1
